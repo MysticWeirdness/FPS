66e4665fbd1303241c8069da770e955e9db942e2
Prescott Stebbins <prescott.stebbins@tooeleschools.org>
2024-03-14 09:43:07 -0600

    I think I did something

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,19 +8,27 @@ public class GameController : MonoBehaviour
     private int targetsShot = 0;
     private float elapsedTime = 0;
     private float totalTime = 30;
+    private const string HIGHSCOREPREFIX = "Highscore\n       ";
 
     [SerializeField] private TextMeshProUGUI targetsText;
     [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private TextMeshProUGUI highscoreText;
+    [SerializeField] private GameObject Menu;
+    [SerializeField]
 
     public void ShotATarget()
     {
         targetsShot++;
     }
 
+    private void Start()
+    {
+        PlayerPrefs.SetInt("Highscore", 0);
+    }
     private void UpdateUI()
     {
         targetsText.text = "Targets: " + targetsShot.ToString();
-        timeText.text = elapsedTime.ToString();
+        timeText.text = "Time Elapsed: " + ((int)elapsedTime).ToString();
     }
 
     private void Update()
@@ -35,7 +43,18 @@ public class GameController : MonoBehaviour
 
     private void Restart()
     {
+        Time.timeScale = 0f;
         targetsShot = 0;
         elapsedTime = 0;
+        Menu.SetActive(true);
+        if(targetsShot > PlayerPrefs.GetInt("Highscore"))
+        {
+            PlayerPrefs.SetInt("Highscore", targetsShot);
+            highscoreText.text = HIGHSCOREPREFIX + targetsShot.ToString();
+        }
+        else
+        {
+            highscoreText.text = HIGHSCOREPREFIX + PlayerPrefs.GetInt("Highscore").ToString();
+        }
     }
 }
